d5855f8c329cfc7558bd5b612843d5a7bd5df213
MrSummat <gonzalomborge@hotmail.com>
2016-11-23 10:01:56 +0000

    Line changed for testing purposes

--- src/main/java/ie/cit/adf/CrowdFundingApplication.java
+++ src/main/java/ie/cit/adf/CrowdFundingApplication.java
@@ -8,6 +8,6 @@ public class CrowdFundingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrowdFundingApplication.class, args);
-		System.out.println("Juanker is alive!");
+		System.out.println("This is a test sentence!");
 	}
 }
