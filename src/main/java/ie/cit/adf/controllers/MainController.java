593b89fca886008ea7ba3a68c6918569e082694f
Dorine BOUTTEN <dorine.boutten@yahoo.fr>
2016-11-23 11:25:27 +0100

    Web page and internationalization with problems

---
+++ src/main/java/ie/cit/adf/controllers/MainController.java
@@ -0,0 +1,17 @@
+package ie.cit.adf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+    public String main(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "main";
+    }
+
+}
