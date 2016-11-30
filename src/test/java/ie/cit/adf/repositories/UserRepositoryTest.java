9a9a2e2f776e74648eb80d13b73fa9b345c1c20d
Pablo García Miranda <uo245530@uniovi.es>
2016-11-30 10:56:35 +0000

    Test of tests working

--- src/test/java/ie/cit/adf/repositories/UserRepositoryTest.java
+++ src/test/java/ie/cit/adf/repositories/UserRepositoryTest.java
@@ -1,25 +1,34 @@
-package ie.cit.adf;
+package ie.cit.adf.repositories;
+
+import java.util.List;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import ie.cit.adf.CrowdFundingApplication;
+import ie.cit.adf.domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = CrowdFundingApplication.class)
 @WebAppConfiguration
 @DirtiesContext(classMode=ClassMode.AFTER_EACH_TEST_METHOD)
-public class CrowdFundingApplicationTests {
+public class UserRepositoryTest {
+	
+	@Autowired
+	private UserRepository userRepository;
 
 	@Test
 	public void contextLoads() {
+		List<User> users = (List<User>) userRepository.findAll();
+		for (User user : users){
+			System.out.println(user.toString());
+		}
 	}
 
 }
